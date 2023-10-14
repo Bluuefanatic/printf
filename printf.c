9e788069733f81c881aea52fafef320d1edfb15b
Bluuefanatic <izirenjoel@gmail.com>
2023-10-14 09:32:36 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ printf.c
@@ -0,0 +1,56 @@
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - A custom printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%' && *(format + 1))
+        {
+            format++;
+
+            switch (*format)
+            {
+                case 'c':
+                    {
+                        char ch = (char)va_arg(args, int);
+                        count += write(1, &ch, 1);
+                    }
+                    break;
+
+                case 's':
+                    count += write(1, va_arg(args, char*), 1);
+                    break;
+
+                case '%':
+                    count += write(1, "%", 1);
+                    break;
+
+                default:
+                    count += write(1, "%", 1);
+                    count += write(1, format, 1);
+            }
+        }
+        else
+        {
+            count += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
